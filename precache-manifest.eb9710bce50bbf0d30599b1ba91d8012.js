2f9295a5c5616bfa46b348ec11ad9dd5f2ec45d0
Airwaves <prairie5270@gmail.com>
2020-04-19 16:30:04 +0800

    Updates

---
+++ precache-manifest.eb9710bce50bbf0d30599b1ba91d8012.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "636c1c7a1f3397b456e40586dad5ccd2",
+    "url": "/react-interactive-paycard/index.html"
+  },
+  {
+    "revision": "1d90e4871ac3a704380e",
+    "url": "/react-interactive-paycard/static/css/main.baae4456.chunk.css"
+  },
+  {
+    "revision": "32269b10e04224187573",
+    "url": "/react-interactive-paycard/static/js/2.81a9133b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-interactive-paycard/static/js/2.81a9133b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d90e4871ac3a704380e",
+    "url": "/react-interactive-paycard/static/js/main.297d404e.chunk.js"
+  },
+  {
+    "revision": "d36c214d0933da79bc43",
+    "url": "/react-interactive-paycard/static/js/runtime-main.c36a9f00.js"
+  }
+]);
